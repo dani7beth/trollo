f16bea6e3f2119c62eba49ac3045b3c728848a38
dani7beth <danielledona28@gmail.com>
2020-11-26 12:40:27 -0700

    finished crud actions for boards model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#Lets create a couple of users for our database
+3.times do
+    password = Faker::Internet.password(min_length: 6, max_length: 10)
+    user = User.create(
+        email: Faker::Internet.email,
+        password: password,
+        password_confirmation: password
+    )
+end
+#lets create a couple of fake boards for our user to have
+5.times do
+Board.create(
+    name: Faker::Dessert.variety,
+    user_id: rand(1...4) #ruby doesnt include the last number so to include 3 i put 4
+)
+end
+
+
+
